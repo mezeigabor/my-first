a91134ebc2e4c2d0da11e534a17749ef49c80150
mezeigabor <g.mezei87@gmail.com>
2020-05-05 22:00:14 +0200

    End Of S6

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,15 @@ namespace API
           
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+
+            });
 
         }
 
@@ -43,6 +52,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- Infastructure/Data/SpecificationEvaluator.cs
+++ Infastructure/Data/SpecificationEvaluator.cs
@@ -14,6 +14,19 @@ namespace Infastructure.Data
                 query = query.Where(spec.Criteria); // p => p.ProductTypeID == id
             }
 
+            if (spec.OrderBy != null) {
+                query = query.OrderBy(spec.OrderBy); // p => p.ProductTypeID == id
+            }
+
+            if (spec.OrderByDescending != null) {
+                query = query.OrderByDescending(spec.OrderByDescending); // p => p.ProductTypeID == id
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
         
             return query;
