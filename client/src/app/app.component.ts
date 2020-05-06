e774e69e1c8092d301ac61a000c7f389a024e2f6
mezeigabor <g.mezei87@gmail.com>
2020-05-06 21:27:15 +0200

    End of S8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './moduls/product';
+import { IPagination } from './moduls/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+  
+    });
+  }
 }
 
